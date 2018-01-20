5508cbb88341ae062bd092802ab354a5705322f7
IhToN <ihton93@gmail.com>
2018-01-20 03:04:21 +0100

    Regenerated itemThumbs, replacing \\ with a single /

--- tools/update_item_data.py
+++ tools/update_item_data.py
@@ -2,25 +2,27 @@
 import os
 import urllib.request
 import json
-import codecs
 
 data_dir = os.path.join(os.path.dirname(__file__), "..", "data")
 
+export_full_url = False
+
+mainUrl = "http://content.warframe.com/MobileExport/"
+
 urls = [
-    ["http://content.warframe.com/MobileExport/Manifest/", "ExportCustoms.json", "customs.json", "ExportCustoms"],
-    ["http://content.warframe.com/MobileExport/Manifest/", "ExportDrones.json", "drones.json", "ExportDrones"],
-    ["http://content.warframe.com/MobileExport/Manifest/", "ExportFlavour.json", "flavour.json", "ExportFlavour"],
-    ["http://content.warframe.com/MobileExport/Manifest/", "ExportGear.json", "gear.json", "ExportGear"],
-    ["http://content.warframe.com/MobileExport/Manifest/", "ExportKeys.json", "keys.json", "ExportKeys"],
-    ["http://content.warframe.com/MobileExport/Manifest/", "ExportManifest.json", "manifest.json", "Manifest"],
-    ["http://content.warframe.com/MobileExport/Manifest/", "ExportRelicArcane.json", "relicArcane.json",
-     "ExportRelicArcane"],
-    ["http://content.warframe.com/MobileExport/Manifest/", "ExportResources.json", "resources.json", "ExportResources"],
-    ["http://content.warframe.com/MobileExport/Manifest/", "ExportSentinels.json", "sentinels.json", "ExportSentinels"],
-    ["http://content.warframe.com/MobileExport/Manifest/", "ExportUpgrades.json", "upgrades.json", "ExportUpgrades"],
-    ["http://content.warframe.com/MobileExport/Manifest/", "ExportRegions.json", "regions.json", "ExportRegions"],
-    ["http://content.warframe.com/MobileExport/Manifest/", "ExportWarframes.json", "warframes.json", "ExportWarframes"],
-    ["http://content.warframe.com/MobileExport/Manifest/", "ExportWeapons.json", "weapons.json", "ExportWeapons"],
+    [mainUrl + "Manifest/", "ExportCustoms.json", "customs.json", "ExportCustoms"],
+    [mainUrl + "Manifest/", "ExportDrones.json", "drones.json", "ExportDrones"],
+    [mainUrl + "Manifest/", "ExportFlavour.json", "flavour.json", "ExportFlavour"],
+    [mainUrl + "Manifest/", "ExportGear.json", "gear.json", "ExportGear"],
+    [mainUrl + "Manifest/", "ExportKeys.json", "keys.json", "ExportKeys"],
+    [mainUrl + "Manifest/", "ExportManifest.json", "manifest.json", "Manifest"],
+    [mainUrl + "Manifest/", "ExportRelicArcane.json", "relicArcane.json", "ExportRelicArcane"],
+    [mainUrl + "Manifest/", "ExportResources.json", "resources.json", "ExportResources"],
+    [mainUrl + "Manifest/", "ExportSentinels.json", "sentinels.json", "ExportSentinels"],
+    [mainUrl + "Manifest/", "ExportUpgrades.json", "upgrades.json", "ExportUpgrades"],
+    [mainUrl + "Manifest/", "ExportRegions.json", "regions.json", "ExportRegions"],
+    [mainUrl + "Manifest/", "ExportWarframes.json", "warframes.json", "ExportWarframes"],
+    [mainUrl + "Manifest/", "ExportWeapons.json", "weapons.json", "ExportWeapons"],
 ]
 
 itemThumbs = []
@@ -46,7 +48,11 @@ for item in itemThumbs:
     print('Loading image for', item['name'])
     for elem in manifest:
         if elem['uniqueName'] == item['uniqueName']:
-            item.update({'textureLocation': elem['textureLocation']})
+            texture = elem['textureLocation'].replace('\\', '/')
+            if export_full_url:
+                item.update({'textureLocation': mainUrl + texture})
+            else:
+                item.update({'textureLocation': texture})
             break
 
 with open(os.path.join(data_dir, 'itemThumbs.json'), "w") as file:
